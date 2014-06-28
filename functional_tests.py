8a5a4fd8f0f67b32938ce7d4723f8125b6ab6b6a
Jeff Elkner <jeff@elkner.net>
2014-06-28 15:53:49 -0400

    Functional test now checks we can input a to-do item.

--- functional_tests.py
+++ functional_tests.py
@@ -18,19 +18,35 @@ class NewVisiterTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists.
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                input.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
 
         # She types "Finish Khan Academy intro to programming." into a
         # text box.
+        input.send_keys('Finish Khan Academy intro to programming')
 
         # When she hits enter, the page updates, and now the page lists:
         # "1: Finish Khan Academy intro to programming." as an item in a
         # to-do list.
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: Finish Khan Academy intro to programming'
+                    for row in rows)
+        )
 
         # There is still a text box inviting her to add another item. She
         # enters: "Complete Code Academy jQuery course."
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list.
 
