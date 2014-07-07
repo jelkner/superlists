b47e61ad9fb4e388e66500b0a33b01cee17473b2
Jeff Elkner <jeff@elkner.net>
2014-07-06 20:42:55 -0400

    Refactored functional test for existance of list items.

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisiterTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_list_and_retrieve_later(self):
         # Jocelyn has heard we are writing a cool new online to-do app.
         # She goes to check out its homepage...
@@ -31,33 +36,26 @@ class NewVisiterTest(unittest.TestCase):
 
         # She types "Finish Khan Academy intro to programming." into a
         # text box.
-        inputbox.send_keys('Finish Khan Academy intro to programming.')
+        first_text = "Finish Khan Academy intro to programming."
+        inputbox.send_keys(first_text)
 
         # When she hits enter, the page updates, and now the page lists:
         # "1: Finish Khan Academy intro to programming." as an item in a
         # to-do list.
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn('1: Finish Khan Academy intro to programming.',
-                      [row.text for row in rows])
+        self.check_for_row_in_list_table('1: {0}'.format(first_text)) 
 
         # There is still a text box inviting her to add another item. She
         # enters: "Complete Code Academy jQuery course."
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Complete Code Academy jQuery course.')
+        second_text = "Complete Code Academy jQuery course."
+        inputbox.send_keys(second_text)
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        self.check_for_row_in_list_table('1: {0}'.format(first_text)) 
+        self.check_for_row_in_list_table('2: {0}'.format(second_text)) 
 
-        self.assertIn('1: Finish Khan Academy intro to programming.',
-                      [row.text for row in rows])
-        self.assertIn('2: Complete Code Academy jQuary course.',
-                      [row.text for row in rows])
 
         # Jocelyn wonders whether the site will remember her list. Then she
         # sees that the site has generated a unique URL for her site -- there
