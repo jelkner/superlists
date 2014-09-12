feb6f158f852236c942e8ad7a0bc53c54e76d199
Jeffrey Elkner <jeff@elkner.net>
2014-09-12 11:08:39 -0400

    Changed base.py to make tests pass again.

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,8 +1,7 @@
-import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from unittest import skip
+
+import sys
 
 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -28,10 +27,10 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def get_item_input_box(self):
+        return self.browser.find_element_by_id('id_text')
+
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
-
-    def get_item_input_box(self):
-        return self.browser.find_element_by_id('id_text')
